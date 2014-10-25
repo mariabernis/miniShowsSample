1a8c5a78b6f25dc92087bb0596982f00e14d2f8a
Alessandro Martin <martin.acg@gmail.com>
2014-10-26 00:55:04 +0200

    Before adding SevenSwitch

---
+++ MiniShows/Controllers/OptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  OptionsViewController.h
+//  MiniShows
+//
+//  Created by Alessandro on 25/10/14.
+//  Copyright (c) 2014 mariabernis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OptionsViewController : UIViewController
+
+@end
